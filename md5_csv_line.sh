14ce10651acb9cd25567449dc6b6695765302f8b
shenfeng_air <shenfeng0571@gmail.com>
2020-06-23 15:35:48 +0800

    add parameter limit , get the CSV storage path in md5_csv_line.sh

--- md5_csv_line.sh
+++ md5_csv_line.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
+
+if [ $# != 1 ] ; then
+  echo "USAGE: $0 csv_file_name"
+  echo " e.g.: $0 xxx.csv"
+  exit 1;
+fi
+
+# get local csv location
+CSV_FOLDER=`grep "csv_base" connection.cfg | cut -d= -f2`
+
+cd "${CSV_FOLDER}"
+
 CSV_FILE="$@"
 echo "$CSV_FILE"
 
-HASH_FILE="$CSV_FILE"".hash" 
+HASH_FILE="$CSV_FILE"".hash"
 echo ${HASH_FILE}
 
 LOAD_FILE="${CSV_FILE}"".detail"
